7825fac4c5c9e7ceea61b63e5f718993c1e1fb45
joseem <molinetijose@gmail.com>
2024-02-19 16:53:52 -0300

    Update 19/02/24

--- stateful_widget/lib/main.dart
+++ stateful_widget/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(name: 'Gustavo'));
 }
 
 class MyApp extends StatefulWidget {
-  final String name = 'Jonas';
-  const MyApp({Key? key}) : super(key: key);
+  final String name;
+  const MyApp({Key? key, required this.name}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -15,12 +15,25 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int salary = 5000;
 
+  void aumentaSalario(int valorAumento) {
+    setState(() {
+      salary += valorAumento;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'O salário de ${widget.name} é $salary',
-        textDirection: TextDirection.ltr,
+      child: GestureDetector(
+        onTap: () {
+          print('clicou');
+          aumentaSalario(599);
+          print('novo salario ${salary}');
+        },
+        child: Text(
+          'O salário de ${widget.name} é $salary',
+          textDirection: TextDirection.ltr,
+        ),
       ),
     );
   }
